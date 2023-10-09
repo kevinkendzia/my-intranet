699b1934d0bb6a2168fc02a98d461b5b92e4e2bc
Kevin Kendzia <k.kendzia@xdev-software.de>
2023-10-09 22:05:34 +0200

    view-base + homeview erstellt

---
+++ my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/ui/views/home/HomeView.java
@@ -0,0 +1,25 @@
+package de.kkendzia.myintranet.ei.ui.views.home;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import de.kkendzia.myintranet.ei.ui._api.view.AbstractEIView;
+import de.kkendzia.myintranet.ei.ui.layout.MainLayout;
+import de.kkendzia.myintranet.ei.ui.layout.menu.MenuRoute;
+
+@MenuRoute(label = "menu.home")
+@Route(value = "home", layout = MainLayout.class)
+@RouteAlias("")
+public class HomeView extends AbstractEIView<VerticalLayout>
+{
+    public HomeView()
+    {
+        VerticalLayout root = getContent();
+        root.add(new H2(getTranslation("app.title.pre")));
+        root.add(new H1(getTranslation("app.title")));
+        root.add(new Paragraph(getTranslation("app.description")));
+    }
+}
