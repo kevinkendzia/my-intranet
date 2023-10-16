189d542c5017bf039d4aa59386821ad3793f6b44
Kevin Kendzia <k.kendzia@xdev-software.de>
2023-10-16 20:18:42 +0200

    fixed & documented session error

--- my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/core/EIInitListener.java
+++ my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/core/EIInitListener.java
@@ -4,15 +4,30 @@ import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import de.kkendzia.myintranet.ei.ui.errors.EIErrorHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EIInitListener implements VaadinServiceInitListener
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EIInitListener.class);
+
     @Override
     public void serviceInit(ServiceInitEvent event)
     {
         VaadinService service = event.getSource();
-        service.addSessionInitListener(e -> e.getSession().setErrorHandler(new EIErrorHandler()));
+
+        service.addSessionInitListener(e ->
+        {
+            /*
+             * 16.10.2023 KK:
+             * Only triggered on new sessions!
+             * Clear Browser-Data (Cookies) if event isn't triggering!
+             */
+            e.getSession().setErrorHandler(new EIErrorHandler());
+        });
+
+        LOGGER.info("EI-Service initialized!");
     }
 }
