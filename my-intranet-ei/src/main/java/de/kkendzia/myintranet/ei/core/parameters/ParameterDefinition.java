5c5b056e1e10a663859946f9fbb7efc416fea080
Kevin Kendzia <k.kendzia@xdev-software.de>
2023-11-16 22:36:48 +0100

    bugfix

--- my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/core/parameters/ParameterDefinition.java
+++ my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/core/parameters/ParameterDefinition.java
@@ -3,40 +3,20 @@ package de.kkendzia.myintranet.ei.core.parameters;
 import com.vaadin.flow.function.SerializableFunction;
 import com.vaadin.flow.router.QueryParameters;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.vaadin.flow.function.SerializableFunction.identity;
 
-public class ParameterDefinition<T>
+public record ParameterDefinition<T>(
+        String name,
+        SerializableFunction<String, T> parser) implements Serializable
 {
-    private String name;
-    private SerializableFunction<String, T> parser;
-
-    public ParameterDefinition(
-            String name,
-            SerializableFunction<String, T> parser)
-    {
-        this.name = name;
-        this.parser = parser;
-    }
-
     public QueryParameters createQueryParameters(String value)
     {
         return QueryParameters.of(name, value);
     }
-
-    //region SETTER / GETTER
-    public String getName()
-    {
-        return name;
-    }
-
-    public SerializableFunction<String, T> getParser()
-    {
-        return parser;
-    }
-    //endregion
 
     //region STATIC FACTORIES
     public static ParameterDefinition<Long> longParam(String name)
@@ -58,6 +38,7 @@ public class ParameterDefinition<T>
     {
         return new ParameterDefinition<>(name, s -> s == null || s.isEmpty() || Boolean.parseBoolean(s));
     }
+
     public static ParameterDefinition<LocalDate> localDateParam(String name, String pattern)
     {
         return new ParameterDefinition<>(name, x -> LocalDate.parse(x, DateTimeFormatter.ofPattern(pattern)));
