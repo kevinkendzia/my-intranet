635d6ad079e0c9f117b31c19c59b43423562505b
Kevin Kendzia <k.kendzia@xdev-software.de>
2023-11-17 21:52:28 +0100

    optimized home-layout

--- my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/ui/views/home/HomeView.java
+++ my-intranet-ei/src/main/java/de/kkendzia/myintranet/ei/ui/views/home/HomeView.java
@@ -2,10 +2,13 @@ package de.kkendzia.myintranet.ei.ui.views.home;
 
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,15 +20,20 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.router.RouteConfiguration;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
+import com.vaadin.flow.theme.lumo.LumoUtility.FontWeight;
+import com.vaadin.flow.theme.lumo.LumoUtility.Overflow;
 import de.kkendzia.myintranet.ei.core.utils.GridColumnFactory;
 import de.kkendzia.myintranet.ei.core.view.AbstractEIView;
 import de.kkendzia.myintranet.ei.ui.components.menu.provider.annotation.MenuRoute;
+import de.kkendzia.myintranet.ei.ui.components.text.MultiLineTitle;
 import de.kkendzia.myintranet.ei.ui.layouts.main.EIDrawer;
 import de.kkendzia.myintranet.ei.ui.layouts.main.EIMainLayout;
+import de.kkendzia.myintranet.ei.ui.views.home.HomeViewPresenter.ActionItem;
 import de.kkendzia.myintranet.ei.ui.views.home.HomeViewPresenter.NewsItem;
 import jakarta.annotation.security.PermitAll;
 
-import java.util.Objects;
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -37,8 +45,12 @@ public class HomeView extends AbstractEIView<VerticalLayout> implements LocaleCh
 {
     public static final String NAV = "home";
     public static final String MENU_NAME = "menu.home";
-    private final HorizontalLayout hlRow1 = new HorizontalLayout();
-    private final HorizontalLayout hlRow2 = new HorizontalLayout();
+
+    private final MultiLineTitle title = new MultiLineTitle();
+    private final Paragraph description = new Paragraph();
+    private final Row row1 = new Row();
+    private final Row row2 = new Row();
+    private final Component gridLabel = createLabel("");
     private final Grid<NewsItem> grid = new Grid<>();
     private final HomeViewPresenter presenter;
 
@@ -46,40 +58,26 @@ public class HomeView extends AbstractEIView<VerticalLayout> implements LocaleCh
     {
         this.presenter = requireNonNull(presenter, "presenter can't be null!");
 
-        VerticalLayout vlText = new VerticalLayout();
-        vlText.setPadding(false);
-        vlText.add(new H2(getTranslation("app.title.pre")));
-        vlText.add(new H1(getTranslation("app.title")));
-        vlText.add(new Paragraph(getTranslation("app.description")));
-
-//        Image imgHome = new Image(IMG_HOME.getPath(), "HOME");
-//        imgHome.addClassNames("home-image");
-//        imgHome.addClassNames(HIDE_XL, HIDE_L, HIDE_M);
-//        imgHome.setWidth("25em");
-
-        HorizontalLayout hlCenter = new HorizontalLayout();
-        hlCenter.add(vlText);
-//        hlCenter.add(imgHome);
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.setDetailsVisibleOnClick(true);
+        grid.setItemDetailsRenderer(new ComponentRenderer<>(itm -> new Span(itm.message())));
+        grid.setAllRowsVisible(true);
 
         GridColumnFactory.addCollapsedColumn(grid, "Title", NewsItem::title);
         GridColumnFactory.addExpandedColumn(grid, "Message", NewsItem::message);
         GridColumnFactory.addCollapsedColumn(grid, "Seen", NewsItem::seen);
 
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-        grid.setDetailsVisibleOnClick(true);
-        grid.setItemDetailsRenderer(new ComponentRenderer<>(itm -> new Span(itm.message())));
-
         VerticalLayout root = getContent();
-        root.addClassNames("background-image-home");
         root.setSizeFull();
         root.setAlignItems(Alignment.STRETCH);
         root.setHeightFull();
-        root.add(hlCenter);
-        root.add(new H3("Zuletzt"));
-        root.add(hlRow1);
-        root.add(new H3("Favoriten"));
-        root.add(hlRow2);
-        root.add(new H3("News"));
+        root.setPadding(false);
+
+        root.add(title);
+        root.add(description);
+        root.add(row1);
+        root.add(row2);
+        root.add(gridLabel);
         root.addAndExpand(grid);
     }
 
@@ -87,35 +85,20 @@ public class HomeView extends AbstractEIView<VerticalLayout> implements LocaleCh
     protected void onAttach(final AttachEvent attachEvent)
     {
         super.onAttach(attachEvent);
-        presenter
-                .fetchActionItems(false, 0, 5)
-                .map(HomeView::createButton)
-                .filter(Objects::nonNull)
-                .forEach(hlRow1::add);
 
-        presenter
-                .fetchActionItems(true, 0, 5)
-                .map(HomeView::createButton)
-                .filter(Objects::nonNull)
-                .forEach(hlRow2::add);
+        row1.setItems(presenter.fetchActionItems(false, 0, 5).toList());
+        row2.setItems(presenter.fetchActionItems(true, 0, 5).toList());
 
         grid.setItems(presenter.createNewsDataProvider());
     }
 
-    private static Component createButton(final HomeViewPresenter.ActionItem action)
+    private static Component createLabel(String text)
     {
-        return RouteConfiguration.forSessionScope().getRoute(action.route())
-                .map(r ->
-                {
-                    final var b = new Button(action.title(), VaadinIcon.LIST.create());
-                    b.setWidth("10em");
-                    b.setHeight("10em");
-
-                    final var link = new RouterLink();
-                    link.add(b);
-                    link.setRoute(r);
-                    return link;
-                }).orElse(null);
+        final var div = new Div();
+        div.addClassName(FontSize.XLARGE);
+        div.addClassName(FontWeight.BOLD);
+        div.setText(text);
+        return div;
     }
 
     @Override
@@ -123,5 +106,63 @@ public class HomeView extends AbstractEIView<VerticalLayout> implements LocaleCh
     {
         // TODO
         setPageTitle(getTranslation("home.title"));
+        title.setPreTitle(getTranslation("app.title.pre"));
+        title.setTitle(getTranslation("app.title"));
+        description.setText(getTranslation("app.description"));
+        row1.setLabel(getTranslation("previous"));
+        row2.setLabel(getTranslation("favorites"));
+        gridLabel.getElement().setText(getTranslation("news"));
     }
+
+    //region TYPES
+    public static class Row extends Composite<VerticalLayout>
+    {
+        private final Component label = createLabel("");
+        private final HorizontalLayout hl = new HorizontalLayout();
+
+        public Row()
+        {
+            hl.addClassName(Overflow.AUTO);
+
+            final var root = getContent();
+            root.setPadding(false);
+            root.setAlignItems(Alignment.STRETCH);
+            root.add(label);
+            root.add(hl);
+        }
+
+        public void setLabel(String label)
+        {
+            this.label.getElement().setText(label);
+        }
+
+        public void setItems(List<ActionItem> items)
+        {
+            items.stream().map(ActionButton::new).forEach(hl::add);
+        }
+    }
+
+    public static class ActionButton extends Composite<RouterLink>
+    {
+        public ActionButton(ActionItem action)
+        {
+            final var b = new Button(action.title(), VaadinIcon.LIST.create());
+            b.setWidth("10em");
+            b.setHeight("10em");
+
+            final var link = getContent();
+            link.add(b);
+
+            RouteConfiguration
+                    .forSessionScope()
+                    .getRoute(action.route())
+                    .ifPresent(link::setRoute);
+        }
+
+        public void setRoute(Class<Component> route)
+        {
+            getContent().setRoute(route);
+        }
+    }
+    //endregion
 }
